998ff7d9b8f146e24d0d5f74d1cc1cb5bc463ff6
jy <j202344097@gmail.com>
2023-09-25 12:11:32 +0900

    week05 v7

--- week05/test.go
+++ week05/test.go
@@ -5,14 +5,28 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Print("InPut score 입력 : ")
 	reader := bufio.NewReader(os.Stdin)
 	inputScore, err := reader.ReadString('\n') //옵션 2
-	if err != nil {                            // 조건문 들어갔늨데 이해 안감 뭐라는거여
+	if err != nil {                        
 		log.Fatal(err)
 	}
-	fmt.Println((inputScore))
+	inputScore = strings.TrimSpace(inputScore)
+	score, err := strconv.ParseInt(inputScore, 32)
+
+
+
+	if inputScore >= 90 {
+		grade := "A grade"
+	}
+	else {
+		grade := "under A grade"
+	}
+	fmt.Println(inpuinputScore)
+	fmt.Println("you will get", grade)
 }
