612f0329822c6560bcad494704b0b1e33d21ff14
jy <j202344097@gmail.com>
2023-09-25 12:22:22 +0900

    week05 v8

--- week05/test.go
+++ week05/test.go
@@ -4,29 +4,35 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
-	fmt.Print("InPut score 입력 : ")
+	rand.Seed(time.Now().Unix())
+	answer := rand.Intn(100) + 1 //1~100
+
+	fmt.Println("Guess Game ( 1 ~ 100 ) :")
+	fmt.Println(answer)
+
 	reader := bufio.NewReader(os.Stdin)
-	inputScore, err := reader.ReadString('\n') //옵션 2
-	if err != nil {                        
-		log.Fatal(err)
-	}
-	inputScore = strings.TrimSpace(inputScore)
-	score, err := strconv.ParseInt(inputScore, 32)
 
-
-
-	if inputScore >= 90 {
-		grade := "A grade"
+	for i := 0; i < 10; i++ {
+		fmt.Println("You chance :", 10-i)
+		fmt.Print("Guess number : ")
+		inputNumberString, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		inputNumberString = strings.TrimSpace(inputNumberString)
+		inputNumber, err := strconv.Atoi(inputNumberString)
+		if inputNumber < answer {
+			fmt.Println("Ur guess number is lower ghan answer.") //정답이 더 크다
+		} else if inputNumber > answer {
+			fmt.Println("Ur guess number is higher ghan answer.") //정답이 더 작다
+		}
 	}
-	else {
-		grade := "under A grade"
-	}
-	fmt.Println(inpuinputScore)
-	fmt.Println("you will get", grade)
 }
