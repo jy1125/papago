292e23da9e8a5b8b135649e23043b9cc32e0ea7d
jy <j202344097@gmail.com>
2023-09-25 11:34:57 +0900

    week05 v6

--- week05/test.go
+++ week05/test.go
@@ -11,6 +11,8 @@ func main() {
 	fmt.Print("InPut score 입력 : ")
 	reader := bufio.NewReader(os.Stdin)
 	inputScore, err := reader.ReadString('\n') //옵션 2
-	log.Fatal(err)
+	if err != nil {                            // 조건문 들어갔늨데 이해 안감 뭐라는거여
+		log.Fatal(err)
+	}
 	fmt.Println((inputScore))
 }
